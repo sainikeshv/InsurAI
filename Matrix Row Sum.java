2a576774f80fecc174d36703f6b2f32c583dcf02
sainikeshv <sainikeshv2006@gmail.com>
2026-02-11 19:21:14 +0530

    Create Matrix Row Sum.java
    Given a matrix of size N x M, print row-wise sum, separated by a newline.
    
    Input Format
    The first line of input contains N, M - the size of the matrix, followed by N lines each containing M integers - elements of the matrix.
    
    Output Format
    Print the row-wise sum of the matrix, separated by a newline.
    
    Constraints
    1 <= N, M <= 100
    -100 <= ar[i] <= 100
    
    Example
    Input
    2 3
    5 -1 3
    19 8 -5
    
    Output
    7
    22

---
+++ Matrix Row Sum.java
@@ -0,0 +1,31 @@
+PROGRAM:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int m=sc.nextInt();
+       int mat[][]=new int[n][m];
+       for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<m;j++)
+        {
+            mat[i][j]=sc.nextInt();
+        }
+       }
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+          for(int j=0;j<m;j++)
+          {
+            sum+=mat[i][j];
+          }
+          System.out.println(sum);
+         }
+          System.out.println();
+        }
+       }
